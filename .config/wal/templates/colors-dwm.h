50bf4105103009fcd296b03021ef338e4c80e1fe
Althaf <muhammadalthafrahman@gmail.com>
2021-08-30 22:55:19 +0530

    updates configs

---
+++ .config/wal/templates/colors-dwm.h
@@ -0,0 +1,20 @@
+dwm.normfgcolor: {color15}
+dwm.normbgcolor: {color0}
+dwm.normfloatcolor: {color15}
+
+dwm.titlenormfgcolor: {color15}
+dwm.titlenormbgcolor: {color0}
+
+dwm.tagsnormfgcolor: {color15}
+dwm.tagsnormbgcolor: {color0}
+
+dwm.selfgcolor: {color15}
+dwm.selbgcolor: {color2}
+dwm.selbordercolor: {color2}
+dwm.selfloatcolor: {color15}
+
+dwm.titleselfgcolor: {color15}
+dwm.titleselbgcolor: {color2}
+
+dwm.tagsselfgcolor: {color15}
+dwm.tagsselbgcolor: {color2}
